cee5b4ac085ac1ee865104fd692a5cba502371f9
malbiruk <2601074@gmail.com>
2026-01-16 20:15:45 +0400

    add terrademo

--- 01_docker_terraform/terrademo/main.tf
+++ 01_docker_terraform/terrademo/main.tf
@@ -8,6 +8,24 @@ terraform {
 }
 
 provider "google" {
-  project = "project-7620f717-1e5e-458a-80e"
-  region  = "europe-central2"
+  credentials = "./keys/my-creds.json"
+  project     = "project-7620f717-1e5e-458a-80e"
+  region      = "europe-central2"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "project-7620f717-1e5e-458a-80e-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
